329273848fe1d85961d67222eb04e15afbcb8ca7
abhay470 <abhaysaini2305470@gmail.com>
2026-02-21 20:53:14 +0530

    First commit

---
+++ amit.java
@@ -0,0 +1,16 @@
+
+package package1;
+
+public class amit {
+    private int x;
+     private int y;
+   public void setdata(int p,int q){
+x=p;
+y=q;
+}
+ public void display(){
+    System.out.println(x);
+System.out.println(y);
+}
+    }
+
---
+++ sumit.java
@@ -0,0 +1,11 @@
+package package2;
+import package1.*;
+
+public class sumit {
+    public static void main(String[] args) {
+         amit m1= new amit();
+    m1.setdata(5,6);
+    m1.display();
+    }
+   ;
+}
